1843300348c9bb93c35436df8623da2f69ac45a2
Jacob Bollmann <jacob.bollmann@yahoo.com>
2024-01-09 20:47:43 -0600

    added state to card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,23 @@
+// To use state we need hooks
+//import React from 'react';
+//const useState = React.useState;
+
+import {useState} from 'react';
+
 function Card() {
+  const [myName, setMyName] = useState('Default');
+
+  function clickChangeName() {
+    // set name to new name
+    console.log('Test');
+    setMyName('JAKE');
+  }
+
   return (
     <div className="card">
     <button >count is 0</button>
+    <p>{myName}</p>
+    <button onClick={clickChangeName}>Change Name</button>
     <p>
       Edit <code>src/App.jsx</code> and save to test HMR
     </p>
